1af05c649852f9efb0ae0b03b73949a287259bb7
Alex Alemi <alemi@Hippocampus.(none)>
2011-02-01 17:31:44 -0500

    Added a NullTest class to serve as an example.

---
+++ tests/NullTest.py
@@ -0,0 +1,38 @@
+#import BaseTest and ase
+from BaseTest import *
+
+#import bin scripts, and sys for argv
+import sys
+
+#Test specific imports go here
+
+class NullTest(BaseTest):
+    """NullTest does nothing, but serves as an example test."""
+    
+    def __init__(self,potentialname,energy,TestDependencies=[],*args,**kwargs):
+        BaseTest.__init__(self,potentialname,energy,TestDependencies,*args,**kwargs)
+        #self.potential = self.getASEPotentialByName(potentialname)
+
+
+    def TestResults(self):
+        """Required module, the TestResults Module returns a dictionary of result. 
+        
+        of the form { 'NameOfValue' : value, 'NameOfSecondValue' : secondvalue }
+        """
+
+        return {}
+
+
+    def Verify(self):
+        """ Optional verify script to be used to generate a visual output for
+        
+        quick check that everything is going alright"""
+        pass
+            
+        
+        
+if __name__ == '__main__':
+    test = NullTest(sys.argv[1],sys.argv[2],sys.argv[3:])
+    print test.main()
+    
+        
