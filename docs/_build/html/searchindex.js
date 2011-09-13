af5811f1dcf551cfdd0a8b943def7b6b3a035d7c
Alex Alemi <alemi@Cerbo.(none)>
2011-09-12 21:33:30 -0400

    Cleanup and switch to Sphinx for documentation

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,openkim:0,indic:0,content:0,tabl:0,test:0,modul:0,document:0,page:0},objtypes:{},titles:["Welcome to OpenKIM-Tests&#8217;s documentation!"],objnames:{},filenames:["index"]})
